d3d437045897aecc769849b4b12180f0a9c790ac
MarceloVichar <marcos.marcelo43@gmail.com>
2024-02-23 17:05:20 -0300

    atualizado docker compose

--- environment/setup.sh
+++ environment/setup.sh
@@ -2,6 +2,8 @@
 
 docker compose exec api cp .env.example .env
 
+docker compose exec ui cp .env.example .env
+
 docker compose exec api composer install
 docker compose exec api php artisan migrate:fresh --seed
 docker compose exec api php artisan key:generate
